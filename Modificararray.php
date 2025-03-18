d22cfa59f738aa670645f4e25cb6c11a73bc9b34
Iván Vargas <ivancamilovargasgarcia@gmail.com>
2025-03-18 10:41:25 -0500

    Create Modificararray.php

---
+++ Modificararray.php
@@ -0,0 +1,30 @@
+<?php
+	//creacion de un arreglo
+	$cosas = array("tablero", "mesa","compu");
+	
+    $cosas[]="cable";
+
+	print_r($cosas);
+	
+    //eliminar algun valor
+	
+    $eliminarcosa = "tablero";
+
+	
+    $llave = array_search($eiminarcosa, $cosas);
+	
+    //condicion de si debe ejecutar despues de eliminar un objeto
+	
+    if($llave!==false){
+
+	  unset($cosas[$llave]);
+	}
+//print_r($colores);
+
+$cosas[1]="tablero";
+//print_r($colores);
+
+$cosas[3]="compu";
+print_r($cosas);
+
+?>
